ea6a3507430cf43ca03f297a592ccbb407b2bd11
rudyer <42814660+rudyer@users.noreply.github.com>
2020-07-07 16:42:28 -0300

    Add files via upload

--- src/components/newsrow/NewsRow.jsx
+++ src/components/newsrow/NewsRow.jsx
@@ -1,27 +1,55 @@
-import React from 'react'
+import React, { Component } from 'react';
 import './NewsRow.css';
 import NewsCard from '../newscard/NewsCard';
 import LogoLR from '../../images/blackzao.png';
+import firebase from './../../Firebase';
+import { render } from '@testing-library/react';
 
-function NewsRow(){
+class NewsRow extends Component{
+    
+    constructor(props) {
+        super(props);
+        this.ref = firebase.firestore().collection('notes');
+        this.unsubscribe = null;
+        this.state = {
+          boards: []
+        };
+      }
+    
+      onCollectionUpdate = (querySnapshot) => {
+        const boards = [];
+        querySnapshot.forEach((doc) => {
+          const { title, description, author } = doc.data();
+          boards.push({
+            key: doc.id,
+            doc, // DocumentSnapshot
+            title,
+            description,
+            author,
+          });
+        });
+        this.setState({
+          boards
+       });
+      }
+    
+      componentDidMount() {
+        this.unsubscribe = this.ref.onSnapshot(this.onCollectionUpdate);
+      }
+    
+    
+    render(){
     return(
+        
         <div className="Row">
+            {this.state.boards.map(board =>
             <div className="NewsContent">
-                <NewsCard></NewsCard>
+                <NewsCard title = {board.title} description = {board.description}></NewsCard>
                 <img className="LogoLR" src={LogoLR} alt="Logo LR"/>
-                <NewsCard></NewsCard>
-                <NewsCard></NewsCard>
-                <img className="LogoLR" src={LogoLR} alt="Logo LR"/>
-                <NewsCard></NewsCard>
-                <NewsCard></NewsCard>
-                <img className="LogoLR" src={LogoLR} alt="Logo LR"/>
-                <NewsCard></NewsCard>
-                <NewsCard></NewsCard>
-                <img className="LogoLR" src={LogoLR} alt="Logo LR"/>
-                <NewsCard></NewsCard>
             </div>
+            )}
         </div>   
     )
 }
-
+}
 export default NewsRow;
